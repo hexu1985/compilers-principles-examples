6f434290c3155f3cfd74e3145c00eb70123f2267
hexu <hexu_1985@sina.com>
2024-10-14 16:51:37 +0800

    update */ply/stone/ast/__init__.py

--- Homemake.Scripting.Languages.In.Two.Weeks/python/chap06/ply/stone/ast/__init__.py
+++ Homemake.Scripting.Languages.In.Two.Weeks/python/chap06/ply/stone/ast/__init__.py
@@ -4,10 +4,10 @@ from . ASTree import ASTree
 from . BinaryExpr import BinaryExpr
 from . BlockStmnt import BlockStmnt
 from . IfStmnt import IfStmnt
+from . Name import Name
 from . NegativeExpr import NegativeExpr
 from . NullStmnt import NullStmnt
 from . NumberLiteral import NumberLiteral
 from . PrimaryExpr import PrimaryExpr
 from . StringLiteral import StringLiteral
 from . WhileStmnt import WhileStmnt
-from . Name import Name
