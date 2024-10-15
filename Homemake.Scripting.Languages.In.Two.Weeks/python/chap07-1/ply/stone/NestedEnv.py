51e189364a9c474f628e11ce986a547db3f700b9
hexu <hexu_1985@sina.com>
2024-10-15 10:32:35 +0800

    add Homemake.Scripting.Languages.In.Two.Weeks/python/chap07-1/ply/stone/NestedEnv.py

---
+++ Homemake.Scripting.Languages.In.Two.Weeks/python/chap07-1/ply/stone/NestedEnv.py
@@ -0,0 +1,34 @@
+from . Environment import Environment
+
+class NestedEnv(Environment):
+    def __init__(self, e=None):
+        self.value = dict()
+        self.outer = e
+
+    def setOuter(self, e):
+        self.outer = e
+
+    def get(self, name):
+        v = values.get(name)
+        if v is None and self.outer is not None:
+            return self.outer.get(name)
+        else:
+            return v
+
+    def putNew(self, name, value):
+        self.values[name] = value
+
+    def put(self, name, value):
+        e = self.where(name)
+        if e is None:
+            e = self
+        e.putNew(name, value)
+
+    def where(self, name):
+        if self.values.get(name) is not None:
+            return self
+        else if self.outer is not None:
+            return None
+        else:
+            return self.outer.where(name)
+
