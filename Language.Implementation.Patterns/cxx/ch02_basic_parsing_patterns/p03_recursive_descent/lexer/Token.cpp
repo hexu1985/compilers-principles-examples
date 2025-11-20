a31dd60a146112f352366f5b1086ae3a7b6dc40d
hexu <hexu_1985@sina.com>
2025-11-20 22:09:16 +0800

    update Language.Implementation.Patterns/cxx/ch02_basic_parsing_patterns

--- Language.Implementation.Patterns/cxx/ch02_basic_parsing_patterns/p03_recursive_descent/lexer/Token.cpp
+++ Language.Implementation.Patterns/cxx/ch02_basic_parsing_patterns/p03_recursive_descent/lexer/Token.cpp
@@ -3,6 +3,6 @@
 
 std::string Token::toString(const Lexer& lexer) const {
     std::string tname = lexer.getTokenName(type);
-    return "<'" + text + "'," + tname;
+    return "<'" + text + "'," + tname + ">";
 }
 
