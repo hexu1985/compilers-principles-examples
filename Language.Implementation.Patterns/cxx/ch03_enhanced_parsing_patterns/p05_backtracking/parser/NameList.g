3470c9e1f1e80915f5afda7e1d55a6ca2c226739
hexu <hexu_1985@sina.com>
2025-12-29 17:21:38 +0800

    update ch03_enhanced_parsing_patterns/p05_backtracking/parser

--- Language.Implementation.Patterns/cxx/ch03_enhanced_parsing_patterns/p05_backtracking/parser/NameList.g
+++
@@ -1,6 +0,0 @@
-list     : '[' elements ']' ; // match bracketed list
-elements : element (',' element)* ; // match comma-separated list
-element  : NAME '=' NAME // match assignment such as a=b
-         | NAME
-         | list
-         ;
--- Language.Implementation.Patterns/cxx/ch03_enhanced_parsing_patterns/p05_backtracking/parser/Parser.hpp
+++ Language.Implementation.Patterns/cxx/ch03_enhanced_parsing_patterns/p05_backtracking/parser/Parser.hpp
@@ -3,26 +3,34 @@
 #include "lexer/Token.hpp"
 #include "lexer/Lexer.hpp"
 
-class ParserError: public std::logic_error {
-public:
-    ParserError(const std::string& what_arg): std::logic_error(what_arg) {}
-};
-
 class Parser {
 public:
-	Parser(Lexer* input, int k);
+	Parser(Lexer* input);
 	
 	void consume(); 
+
+    void sync(int i);
+
+    void fill(int n);
 
     Token LT(int i);
 
     int LA(int i);
 
 	void match(int x); 
+
+    int mark();
+
+    void release();
 	
+    void seek(int index);
+
+    bool isSpeculating();
+
 protected:
 	Lexer* input;
-    std::vector<Token> lookahead;
-    int k;
-    int p = 0;
+    std::vector<int> markers;       // stack of index markers into lookahead buffer
+    std::vector<Token> lookahead;   // dynamically-sized lookahead buffer
+    int p = 0;  // index of current token in lookahead buffer
+                // LT(1) returns lookahead[p] 
 };
