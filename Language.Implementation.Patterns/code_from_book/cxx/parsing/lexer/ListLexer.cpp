d298de658cdf1cf590d5996fce4fdef7c178eb8e
hexu <hexu_1985@sina.com>
2026-02-13 15:50:29 +0800

    update Language.Implementation.Patterns/code_from_book/cxx/parsing/lexer: 迁移到unique_ptr

--- Language.Implementation.Patterns/code_from_book/cxx/parsing/lexer/ListLexer.cpp
+++ Language.Implementation.Patterns/code_from_book/cxx/parsing/lexer/ListLexer.cpp
@@ -1,3 +1,8 @@
 #include "ListLexer.hpp"
 
+const int ListLexer::NAME;
+const int ListLexer::COMMA;
+const int ListLexer::LBRACK;
+const int ListLexer::RBRACK;
+
 const char* ListLexer::tokenNames[] = {"n/a", "<EOF>", "NAME", "COMMA", "LBRACK", "RBRACK"};
