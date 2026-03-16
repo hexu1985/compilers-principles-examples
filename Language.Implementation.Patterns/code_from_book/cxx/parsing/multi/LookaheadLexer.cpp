2bc9731e99137dbf9df3db938d1b1f729c734cca
hexu <hexu_1985@sina.com>
2026-03-16 16:39:48 +0800

    add Language.Implementation.Patterns/code_from_book/cxx/parsing/multi/LookaheadLexer.*

---
+++ Language.Implementation.Patterns/code_from_book/cxx/parsing/multi/LookaheadLexer.cpp
@@ -0,0 +1,8 @@
+#include "LookaheadLexer.hpp"
+
+const int LookaheadLexer::NAME;
+const int LookaheadLexer::COMMA;
+const int LookaheadLexer::LBRACK;
+const int LookaheadLexer::RBRACK;
+
+const char* LookaheadLexer::tokenNames[] = {"n/a", "<EOF>", "NAME", "COMMA", "LBRACK", "RBRACK"};
