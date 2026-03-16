0716659cd8688f89ec49742eae32aa8088923359
hexu <hexu_1985@sina.com>
2026-03-16 17:27:18 +0800

    update Language.Implementation.Patterns/code_from_book/cxx/parsing/multi/Parser.hpp

--- Language.Implementation.Patterns/code_from_book/cxx/parsing/multi/Parser.hpp
+++ Language.Implementation.Patterns/code_from_book/cxx/parsing/multi/Parser.hpp
@@ -8,27 +8,38 @@
 
 class Parser {
 protected:
-    std::unique_ptr<Lexer> input;     // from where do we get tokens?
-    Token lookahead;                  // the current lookahead token
+    std::unique_ptr<Lexer> input;   // from where do we get tokens?
+    std::vector<Token> lookahead;   // circular lookahead buffer
+    int k;                          // how many lookahead symbols
+    int p = 0;                      // circular index of next token position to fill
 
 public:
-    Parser(std::unique_ptr<Lexer> input_) : input(std::move(input_)) {
-        consume();
+    Parser(std::unique_ptr<Lexer> input_, int k_) : input(std::move(input_)), k(k_) {
+        lookahead.resize(k);                // make lookahead buffer
+        for (int i=1; i<=k; i++) consume(); // prime buffer with k lookahead
     }
 
     virtual ~Parser() = 0;
 
-    /** If lookahead token type matches x, consume & return else error */
-    void match(int x) {
-        if (lookahead.type == x) consume();
-        else {
-            throw std::runtime_error("expecting " + input->getTokenName(x) +
-                                     "; found " + lookahead.toString());
-        }
+    void consume() {
+        lookahead[p] = input->nextToken();  // fill next position with token
+        p = (p+1) % k;                      // fill next position with token
     }
 
-    void consume() {
-        lookahead = input->nextToken();
+    Token LT(int i) {
+        return lookahead[(p+i-1) % k];      // circular fetch
+    }
+
+    int LA(int i) {
+        return LT(i).type;
+    }
+
+    void match(int x) {
+        if ( LA(1) == x) consume();
+        else {
+            throw std::runtime_error("expecting " + input->getTokenName(x) +
+                                     "; found " + LT(1).toString());
+        }
     }
 };
 
