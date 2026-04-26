4f1f14a277934f652c49b3e5878bf5db4c45ec30
hexu <hexu_1985@sina.com>
2026-04-26 15:07:12 +0800

    update ExprJoyRide.cpp

--- The.Definitive.ANTLR4.Reference/cxx/tour/Expr/ExprJoyRide.cpp
+++ The.Definitive.ANTLR4.Reference/cxx/tour/Expr/ExprJoyRide.cpp
@@ -7,17 +7,24 @@
 
 using namespace antlr4;
 
+std::unique_ptr<ANTLRFileStream> makeANTLRFileStream(const std::string& fileName) {
+    auto input = std::make_unique<ANTLRFileStream>();
+    input->loadFromFile(fileName);
+    return input;
+}
+
 int main(int argc, const char* argv[]) {
     std::string inputFile;
     if (argc > 1) {
         inputFile = argv[1];
     }
 
+#if 0
     std::unique_ptr<std::istream> inputStream;
     if (!inputFile.empty()) {
         // Read from file
         inputStream = std::make_unique<std::ifstream>(inputFile);
-        if (!dynamic_cast<std::ifstream*>(inputStream.get())->good()) {
+        if (!inputStream->good()) {
             std::cerr << "Could not open file: " << inputFile << std::endl;
             return 1;
         }
@@ -28,6 +35,17 @@ int main(int argc, const char* argv[]) {
 
     ANTLRInputStream input(*inputStream);
     ExprLexer lexer(&input);
+#else
+    std::unique_ptr<ANTLRInputStream> input;
+    if (!inputFile.empty()) {
+        input = makeANTLRFileStream(inputFile);
+    } else {
+        input = std::make_unique<ANTLRInputStream>(std::cin);
+    }
+
+    ExprLexer lexer(input.get());
+#endif
+
     CommonTokenStream tokens(&lexer);
     ExprParser parser(&tokens);
     tree::ParseTree* tree = parser.prog();  // parse; start at prog
