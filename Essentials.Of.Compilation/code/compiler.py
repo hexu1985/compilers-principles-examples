6e09c8b3cab1890dc27a1beb863e3a9bd7f4802a
hexu <hexu_1985@sina.com>
2026-05-02 16:57:42 +0800

    add Essentials.Of.Compilation/code

---
+++ Essentials.Of.Compilation/code/compiler.py
@@ -0,0 +1,84 @@
+import ast
+from ast import *
+from utils import *
+from x86_ast import *
+import os
+from typing import List, Tuple, Set, Dict
+
+Binding = Tuple[Name, expr]
+Temporaries = List[Binding]
+
+
+class Compiler:
+
+    ############################################################################
+    # Remove Complex Operands
+    ############################################################################
+
+    def rco_exp(self, e: expr, need_atomic : bool) -> Tuple[expr, Temporaries]:
+        # YOUR CODE HERE
+        pass        
+
+    def rco_stmt(self, s: stmt) -> List[stmt]:
+        # YOUR CODE HERE
+        pass        
+
+    def remove_complex_operands(self, p: Module) -> Module:
+        # YOUR CODE HERE
+        pass                
+
+    ############################################################################
+    # Select Instructions
+    ############################################################################
+
+    # The expression e passed to select_arg should furthermore be an atom.
+    # (But there is no type for atoms, so the type of e is given as expr.)
+    def select_arg(self, e: expr) -> arg:
+        # YOUR CODE HERE
+        pass        
+
+    def select_stmt(self, s: stmt) -> List[instr]:
+        # YOUR CODE HERE
+        pass        
+
+    def select_instructions(self, p: Module) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
+    ############################################################################
+    # Assign Homes
+    ############################################################################
+
+    def assign_homes_arg(self, a: arg, home: Dict[Variable, arg]) -> arg:
+        # YOUR CODE HERE
+        pass        
+
+    def assign_homes_instr(self, i: instr,
+                           home: Dict[Variable, arg]) -> instr:
+        # YOUR CODE HERE
+        pass        
+
+    def assign_homes(self, p: X86Program) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
+    ############################################################################
+    # Patch Instructions
+    ############################################################################
+
+    def patch_instr(self, i: instr) -> List[instr]:
+        # YOUR CODE HERE
+        pass        
+
+    def patch_instructions(self, p: X86Program) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
+    ############################################################################
+    # Prelude & Conclusion
+    ############################################################################
+
+    def prelude_and_conclusion(self, p: X86Program) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
---
+++ Essentials.Of.Compilation/code/run-tests.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+sys.path.append('../python-student-support-code')
+sys.path.append('../python-student-support-code/interp_x86')
+
+import compiler
+import interp_Lvar
+import type_check_Lvar
+from utils import run_tests, run_one_test, enable_tracing
+from interp_x86.eval_x86 import interp_x86
+
+enable_tracing()
+
+compiler = compiler.Compiler()
+
+typecheck_Lvar = type_check_Lvar.TypeCheckLvar().type_check
+
+typecheck_dict = {
+    'source': typecheck_Lvar,
+    'remove_complex_operands': typecheck_Lvar,
+}
+interpLvar = interp_Lvar.InterpLvar().interp
+interp_dict = {
+    'remove_complex_operands': interpLvar,
+    'select_instructions': interp_x86,
+    'assign_homes': interp_x86,
+    'patch_instructions': interp_x86,
+}
+
+if True:
+    run_tests('var', compiler, 'var',
+              typecheck_dict,
+              interp_dict)
+else:
+    run_one_test(os.getcwd() + '/tests/var/zero.py',
+                 'var',
+                 compiler,
+                 'var',
+                 typecheck_dict,
+                 interp_dict)
+
