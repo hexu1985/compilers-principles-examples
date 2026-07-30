9fbae4817e302d40b434999cea5c8664324bdcbc
hexu <hexu_1985@sina.com>
2026-07-30 17:39:35 +0800

    add Language.Implementation.Patterns/cxx/symtab/nested/BaseScope.hpp

--- Language.Implementation.Patterns/cxx/symtab/nested/SymbolTable.hpp
+++ Language.Implementation.Patterns/cxx/symtab/nested/SymbolTable.hpp
@@ -3,56 +3,23 @@
 #include <string>
 #include <unordered_map>
 
-#include "Scope.hpp"
+#include "GlobalScope.hpp"
 #include "Symbol.hpp"
 #include "BuiltInTypeSymbol.hpp"
 
-class SymbolTable : public Scope { // single-scope symtab
-private:
-    std::unordered_map<std::string, Symbol*> symbols;
+class SymbolTable {
+public:
+    GlobalScope* globals = nullptr;
     
 protected:
     void initTypeSystem() {
-        define(new BuiltInTypeSymbol("int"));
-        define(new BuiltInTypeSymbol("float"));
+        globals->define(new BuiltInTypeSymbol("int"));
+        globals->define(new BuiltInTypeSymbol("float"));
+        globals->define(new BuiltInTypeSymbol("void")); // pseudo-type
     }
     
 public:
-    SymbolTable() {
+    SymbolTable(): globals(new GlobalScope) {
         initTypeSystem();
-    }
-
-    ~SymbolTable() {
-        for (auto& item : symbols) {
-            Symbol* symbol = item.second;
-            delete symbol;
-        }
-        symbols.clear();
-    }
-    
-    // Satisfy Scope interface
-    std::string getScopeName() const override { return "global"; }
-    
-    Scope* getEnclosingScope() override { return nullptr; }
-    
-    void define(Symbol* sym) override { 
-        symbols[sym->getName()] = sym; 
-    }
-    
-    Symbol* resolve(const std::string& name) override { 
-        auto it = symbols.find(name);
-        if (it != symbols.end()) {
-            return it->second;
-        }
-        return nullptr;
-    }
-    
-    std::string toString() const {
-        std::string result = getScopeName() + ":{";
-        for (const auto& pair : symbols) {
-            result += pair.first + "=" + pair.second->toString() + ", ";
-        }
-        result += "}";
-        return result;
     }
 };
